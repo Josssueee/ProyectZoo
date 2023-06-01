ca5ae4d29867a495963311dd93a3ed52bf8cfc4e
Josssueee <jcamposecom1@miumg.edu.gt>
2023-06-01 10:57:02 -0600

    La tarea de zoológico

---
+++ src/ProyectZoo.java
@@ -0,0 +1,11 @@
+public class ProyectZoo {
+    public static void main(String[] args) {
+        String user =  "postgres";
+        String pass =  "root";
+         try{
+
+         }catch (Exception e){
+
+         }
+    }
+}
